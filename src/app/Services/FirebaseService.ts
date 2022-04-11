7ba2b0b43666750aeebe0f69ec73f37b02499db9
Victor Elias Barrera Florez <vbarrera@merqueo.com>
2022-04-11 17:54:24 -0500

    fix: Solucionado error caché git y eliminado .env

--- src/app/Services/FirebaseService.ts
+++
@@ -1,7 +0,0 @@
-import admin  from "firebase-admin";
-import ConfigHelper from "../Helpers/ConfigHelper";
-
-const firebaseService = admin.initializeApp({
-    credential: admin.credential.cert(new ConfigHelper().serviceAccount())
-});
-export { firebaseService };
